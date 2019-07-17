f1da9cbc3135f99e50b2798c43b9285f194aa44b
Matt Williams <matt.seongjun@gmail.com>
2019-07-17 10:53:52 -0400

    Add mocha-multi and finish

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,33 +1,62 @@
 import React from "react";
 
 class LoginForm extends React.Component {
-  constructor() {
-    super();
+    constructor(props) {
+        super(props);
+        this.state = {
+            username: "",
+            password: ""
+        };
+    }
 
-    this.state = {};
-  }
+    handleChange = event => {
+        this.setState({
+          [event.target.name]: event.target.value
+        })
+    }
 
-  render() {
-    return (
-      <form>
-        <div>
-          <label>
-            Username
-            <input id="username" name="username" type="text" />
-          </label>
-        </div>
-        <div>
-          <label>
-            Password
-            <input id="password" name="password" type="password" />
-          </label>
-        </div>
-        <div>
-          <button type="submit">Log in</button>
-        </div>
-      </form>
-    );
-  }
+    handleSubmit = event => {
+        event.preventDefault()
+        if (!!this.state.username && !!this.state.password) {
+            this.props.handleLogin(this.state)
+        } else {
+            return
+        }
+    }
+
+    render() {
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <div>
+                    <label>
+                        Username
+                        <input
+                            id="username"
+                            name="username"
+                            type="text"
+                            value={this.state.username}
+                            onChange={this.handleChange}
+                        />
+                    </label>
+                </div>
+                <div>
+                    <label>
+                        Password
+                        <input
+                            id="password"
+                            name="password"
+                            type="password"
+                            value={this.state.password}
+                            onChange={this.handleChange}
+                        />
+                    </label>
+                </div>
+                <div>
+                    <button type="submit">Log in</button>
+                </div>
+            </form>
+        );
+    }
 }
 
 export default LoginForm;
--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -1,20 +1,37 @@
 import React from "react";
 
 class TwitterMessage extends React.Component {
-  constructor() {
-    super();
+    constructor(props) {
+        super(props);
 
-    this.state = {};
-  }
+        this.state = {
+            value: "",
+            remaining: props.maxChars
+        };
+    }
 
-  render() {
-    return (
-      <div>
-        <strong>Your message:</strong>
-        <input type="text" name="message" id="message" />
-      </div>
-    );
-  }
+    handleChange = (event) => {
+        this.setState({
+            value: event.target.value,
+            remaining: this.props.maxChars - event.target.value.length
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                <strong>Your message:</strong>
+                <p>{this.state.remaining}/{this.props.maxChars}</p>
+                <input
+                    type="text" 
+                    name="message" 
+                    id="message" 
+                    value={this.state.value}
+                    onChange={this.handleChange}
+                />
+            </div>
+        );
+    }
 }
 
 export default TwitterMessage;
